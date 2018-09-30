7bdf41b9363bc715d25208beee03ede097c07df8
Ugiy <43519843+Lugiy@users.noreply.github.com>
2018-09-30 22:10:46 +0700

    forml awal

--- formawal.php
+++ formawal.php
@@ -1,5 +1,34 @@
-<?php
+<body bgcolor="#f9f1c5">
+<?php 
+	session_start();
+	error_reporting(0);
+		$user = array("user" => "septimaudip",
+					  "pass" =>"1234567" );
+	if (isset($_POST['submit'])) {
+		if ($_POST['username']==$user['user'] && $_POST['password'] == $user['pass']) {
+			$_SESSION["username"]=$_POST['username'];
+			echo "<h1>APLIKASI SUKA-SUKA</h1><br>";
+			echo "Anda Berhasil login $_POST[username]<br>";
+			echo "Silahkan isi form terlebih dahulu untuk mengetahui suka-suka anda. <a href ='proses.php'>Klik isi form</a>";
+			# code...
+		}else{
+			display_login_form();
+			echo "<p.Username atau password tidak benar</p>";
+		}
+		# code...
+	}else{
+		display_login_form();
+	}
+	function display_login_form(){ ?>
+		<?php echo "<h1> LOGIN </h1><br>"; ?>
+		<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method='POST'>
+		<label for="username">Username </label>
+		: <input type="text" name="username" id="username"><br><br>
+		<label for="password">Password</label>
+		: <input type="password" name="password" id="password"><br><br>
+		<input type="submit" name="submit" value="Login">	
+		</form>
 
+	<?php } ?>	
 
-
-?>
+<body bgcolor="#f9f1c5">
