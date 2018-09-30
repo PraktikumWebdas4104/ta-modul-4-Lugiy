a47d7e0782e102103083bbdc9537c3ba80a7a71b
Ugiy <43519843+Lugiy@users.noreply.github.com>
2018-09-30 22:19:50 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -3,19 +3,19 @@
 
 	<form action="" method="POST" enctype="multipart/form-data">
 		Gambar : <input type="file" name="gambar" width="50px"><br>
-		Hobbi : <br><input type="checkbox" name="hobi[]" value="Bersepeda">Bersepeda<br>
-				<input type="checkbox" name="hobi[]" value="Olahraga">Olahraga<br>
+		Hobbi : <br><input type="checkbox" name="hobi[]" value="Bermain Game">Bermain Game<br>
+				<input type="checkbox" name="hobi[]" value="Berenang">Berenang<br>
 				<input type="checkbox" name="hobi[]" value="Menggambar">Menggambar<br>
 				<input type="checkbox" name="hobi[]" value="Menulis">Menulis<br>
-				<input type="checkbox" name="hobi[]" value="Ngoding">Ngoding<br><br>	
+				<input type="checkbox" name="hobi[]" value="Mengambar">Menggambar<br><br>	
 		Genre film : <br><input type="checkbox" name="film[]" value="Horor">Horor<br>
 				<input type="checkbox" name="film[]" value="Action">Action<br>
-				<input type="checkbox" name="film[]" value="Anime">Anime<br>
+				<input type="checkbox" name="film[]" value="Crime">Crime<br>
 				<input type="checkbox" name="film[]" value="Thriller">Thriller<br>
-				<input type="checkbox" name="film[]" value="Animasi">Animasi<br><br>
+				<input type="checkbox" name="film[]" value="Drama">Drama<br><br>
 		Tempat Wisata Tujuan Travelling : <br><input type="checkbox" name="wisata[]" value="Bali">Bali<br>
-				<input type="checkbox" name="wisata[]" value="Raja Ampat">Raja Ampat<br>
-				<input type="checkbox" name="wisata[]" value="Pulau Derawan">Pulau Derawan<br>
+				<input type="checkbox" name="wisata[]" value="Tokyo Tower">Tokyo Tower<br>
+				<input type="checkbox" name="wisata[]" value="Gedung Sate">Gedung sate<br><br
 				<input type="checkbox" name="wisata[]" value="Bangka Belitung">Bangka Belitung<br>
 				<input type="checkbox" name="wisata[]" value="Labuan Bajo">Labuan Bajo<br><br>	    	    
 
